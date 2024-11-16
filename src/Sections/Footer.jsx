6b015aca78416624098d52e39ccfeca638a7887a
salman <you@example.com>
2024-11-16 20:25:41 +0530

    update footer

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -3,6 +3,11 @@ import { FaArrowUp, FaCopyright } from "react-icons/fa6";
 import { Link } from "react-scroll";
 
 const Footer = () => {
+  // Function to ensure the first scroll always works
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <div
@@ -19,9 +24,10 @@ const Footer = () => {
         id="icon-box"
         className="bg-white text-blue-500 p-3 rounded-full hover:bg-blue-500
               hover:text-white cursor-pointer fixed lg:bottom-6 right-6 bottom-6"
+        onClick={scrollToTop} // Force scroll to top
       >
-        <Link to="home" spy={true} offset={-100} smooth={true}>
-          <FaArrowUp className="size-6 " />
+        <Link to="home" spy={true} offset={0} smooth={true} duration={500}>
+          <FaArrowUp className="size-5 " />
         </Link>
       </div>
     </>
